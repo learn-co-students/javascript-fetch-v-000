51d452cacf9473a57add366d958180879dd38434
Yechiel K <chilikkal@gmail.com>
2017-01-01 11:35:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much.";
+
+const token = '8d36a66a88e84954fc881b446084ef81e2eb73c0'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
