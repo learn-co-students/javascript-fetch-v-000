a1276aaee7ab3468577c26f575bc039e2d7b98f7
BJ Cantlupe <bjcantlupe@gmail.com>
2017-03-08 09:50:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const token = "fd769e261d28f96789d7151242acc05f8f078b9c"
-
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
   }
 }).then(res => res.json()).then(json => console.log(json));
-
-//fd769e261d28f96789d7151242acc05f8f078b9c
