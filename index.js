7c875cff9851669c0e7bb9129b4127508c47ebb3
smithwebtek <brad@smithwebtek.com>
2017-03-13 21:18:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-const app = "I don't do much.";
+
+const token = 'eadb7d5fb1fca4cef7632d91581d2411ef14d699'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
+
+
+// fetch('https://api.github.com/user/repos')
+//   .then(res => res.json())
+//   .then(json => console.log(json));
+
+
+
+// fetch('https://api.github.com/repos/jquery/jquery/commits')
+//   .then(res => res.json())
+//   .then(json => console.log(json));
+
+
+
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+// xhr.responseType = 'json';
+ 
+// xhr.onload = function() {
+//    console.log(xhr.response);
+// };
+ 
+// xhr.onerror = function() {
+//   console.log('Booo');
+// };
+ 
+// xhr.send();
