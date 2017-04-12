bbee08a7d1e7770810d8db121d39a27a83cb8c5c
mcclains2003 <mcclains2003@gmail.com>
2017-04-12 09:02:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much.";
 
-const token = 'b57b8ed4f0ed381874775cd60b9a333e61144d1e'
+const token = 'removed'
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
