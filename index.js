6a81e8755c5433a1963cf00d1d26402cc0bce763
jminterwebs <jmverdone@gmail.com>
2017-05-30 19:53:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much.";
 
 
-const token = 'dc50a8b6de464c356f73b159610fed21a71f3901'
+const token = ''
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
