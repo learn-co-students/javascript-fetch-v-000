505b978293d6bdfa1e445d28271f5d4a76704437
WilliamMena <williammena1991@gmail.com>
2017-06-01 23:12:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const app = "I don't do much.";
+// github token 8d9ff7d58b37676a4b327f8b6ec0b2b3c4f01bc1
+// const app = "I don't do much.";
+const token = '8d9ff7d58b37676a4b327f8b6ec0b2b3c4f01bc1'
+fetch("https://api.github.com/user/repos", {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
