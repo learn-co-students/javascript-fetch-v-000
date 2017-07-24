f76913f0a88201d4d6c6bd284f1b91c06b00045e
Howard DeVennish <howarddevennish@gmail.com>
2017-07-24 12:36:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much.";
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+// xhr.responseType = 'json';
+//
+// xhr.onload = function() {
+//   console.log(xhr.response);
+// };
+//
+// xhr.onerror = function() {
+//   console.log('Booo');
+// };
+//
+// xhr.send();
+// d486264c4e6b62ec9381d81e874c0d9b1b23b73d
+const token = 'd486264c4e6b62ec9381d81e874c0d9b1b23b73d'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
