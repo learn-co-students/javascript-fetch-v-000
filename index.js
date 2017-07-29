e6f86a94dd25d7e549f2c03b5636c206783df1d7
github username apalski <github email address annette.drapalski@gmail.com>
2017-07-29 18:15:16 +1000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const app = "I don't do much.";
+
+const token = '7470c9d6162db8f4a1a7c2f68f068a2b6c58766e'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
