f96638a67ddeb309bdb8ac4fb35c2b10ebedf3c2
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-08-12 17:49:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much.";
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+// xhr.responseType = 'json';
+//
+// xhr.onload = function() {
+//   console.log(xhr.response);
+// };
+//
+// xhr.onerror = function() {
+//   console.log('Booo');
+// };
+//
+// xhr.send();
+//LONG WAY TO REQUEST
+
+fetch('https://api.github.com/user/repos').
+then(res => res.json()).
+then(json => console.log(json))
+
+
+const token = 'ebc9f3377e3f49d92a343527542175fc114d2be9'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
