bf42339743aabe22486eb3c691288d0592c57e03
lawrend <douglas_lawrence@mac.com>
2017-08-23 02:58:26 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const app = "I don't do much.";
+// Generate a token at the github site with the 'repo' scope and use it to authorize access to the API
+const token = '69d7eaeb5439fa303a429c542b4f28a992d72eea'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
+
+
