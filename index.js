ddf23c96578f4c63d68e87e0dd3201718bd190b9
Aditi Green <asaki444@gmail.com>
2017-09-06 00:32:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much.";
+
+
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(res => res.json())
+  .then(json => console.log(json));
+
+const token = 'YOUR_TOKEN_HERE'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${'66b5f749b32b35f9ba0a3c50df52f6479e8bde6a'}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
