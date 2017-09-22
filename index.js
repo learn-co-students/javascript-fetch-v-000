dcde2c5a9a4f19ad00a6427cd201f33ca7bef222
Joe <jsbenning4@gmail.com>
2017-09-21 23:15:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much.";
+// 
+const fake_token = 'nothteactualtokeniquiufkjkfkhj';
+
+
+
+function fetchy() {
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${fake_token}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
+}
