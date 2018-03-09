2e1ccd7336a24f43d7840893b5ac859cb07cd53f
BuffaloRob <peckham.rob@gmail.com>
2018-03-09 10:35:38 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much.";
+
+const token = "ADD YOUR TOKEN HERE";
+
+fetch("https://api.github.com/users/repos", {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(response => response.json())
+  .then(json => console.log(json));
